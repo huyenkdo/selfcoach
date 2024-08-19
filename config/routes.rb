ba976dcb85c313915500ab4ae52d90de455cb9e4
Huyen Do <huyenkdo@gmail.com>
2024-08-19 16:06:29 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#welcome"
+  resources :users, only: [:new, :create]
+  resources :programs, only: [:new, :create, :edit, :update] do
+    member do
+      get :recap
+      resources :sessions, only: [:index, :edit, :update]
+    end
+  end
+  get "/home", to: "pages#home", as: "home"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
