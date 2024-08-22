b602abac5dcf2513df93b61a2b4df1f5fc3a184e
meryem hassaine <hass.meryem@gmail.com>
2024-08-22 10:46:37 +0200

    schema a jour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_20_141821) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_19_132958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_20_141821) do
     t.integer "run_interval_nbr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "date"
   end
 
   create_table "users", force: :cascade do |t|
