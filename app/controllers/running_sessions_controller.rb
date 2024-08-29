a4dc7aa06d1a421b96d8c55baebae6ec141bc68b
Huyen Do <huyenkdo@gmail.com>
2024-08-29 12:58:27 +0200

    calendar done

--- app/controllers/running_sessions_controller.rb
+++ app/controllers/running_sessions_controller.rb
@@ -11,13 +11,13 @@ class RunningSessionsController < ApplicationController
     @user = current_user
     # @program = Program.find(params[:id])
     @running_session = RunningSession.find(params[:id])
-    run = @running_session.run
-    @kind = run.kind
-    @run_interval_km = run.run_interval_km
-    @rest_interval_time = run.rest_interval_time
-    @run_interval_nbr = run.run_interval_nbr
-    @run_interval_pace = run.run_interval_pace
-    @run_interval_time = run.run_interval_time
+    @run = @running_session.run
+    @kind = @run.kind
+    @run_interval_km = @run.run_interval_km
+    @rest_interval_time = @run.rest_interval_time
+    @run_interval_nbr = @run.run_interval_nbr
+    @run_interval_pace = @run.run_interval_pace
+    @run_interval_time = @run.run_interval_time
     @date = @running_session.date
     @program = @running_session.program
     if @rest_interval_time.nil?
